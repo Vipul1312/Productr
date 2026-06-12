836aafaacfb303ce4f1c422dbc6a55d651379e30
Vipul1312 <vipulgupta1312002@gmail.com>
2026-06-12 20:30:22 +0530

    debug: cloudinary env check

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -8,6 +8,11 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+});
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
